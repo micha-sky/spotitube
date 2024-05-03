67ba9820793419db423e4f42875d22923d1a7815
Alex Michalsky <michalsky@env.kiev.ua>
2024-05-03 07:20:51 +0200

    Update spotitube.py

--- spotitube.py
+++ spotitube.py
@@ -1,13 +1,14 @@
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 
 client_id = os.getenv('SPOTIPY_CLIENT_ID')
-
+client_secret = os.getenv('SPOTIPY_CLIENT_SECRET')
 
 # create a Spotify API client using Spotipy library
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret="",
+                                               client_secret=client_secret,
                                                redirect_uri="http://localhost/",
                                                scope="user-library-read"
                                                ))
